541623f2f9335cc7ef69dfd9c68fd2d168841b61
todd-uams <todd@uams.edu>
2017-11-02 13:46:00 -0500

    Remove ACF bug

--- functions.php
+++ functions.php
@@ -8,32 +8,6 @@
 // Fix ACF Support
 add_filter('_includes/acf-pro/settings/show_admin', '__return_true');
 
-add_action('acf/input/admin_head', 'my_acf_admin_head');
-
-function my_acf_admin_head() {
-
-    ?>
-    <script type="text/javascript">
-    (function($) {
-
-        $(document).ready(function(){
-
-            $('.acf-field-573f70ac9357c .acf-input').append( $('#postdivrich') );
-
-        });
-
-    })(jQuery);
-    </script>
-    <style type="text/css">
-        .acf-field #wp-content-editor-tools {
-            background: transparent;
-            padding-top: 0;
-        }
-    </style>
-    <?php
-
-}
-
 register_sidebar( array(
 'name' => 'Footer Flex Widget',
 'id' => 'footer-flex-widget',
