3c078e1ac6a2cc5f4973a65259980d6012b99633
todd-uams <todd@uams.edu>
2017-11-02 13:06:13 -0500

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,55 @@
+    <div role="contentinfo" class="uams-footer">
+
+        <div id="footer-widgets" class="secondary">
+            <div class="flexible-widgets widget-area container<?php echo footer_widget_area_class( 'footer-flex-widget' ); ?>">
+                <?php
+                if(is_active_sidebar('footer-flex-widget')){
+                dynamic_sidebar('footer-flex-widget');
+                }
+                ?>
+            </div>
+        </div>
+
+
+        <a href="http://www.uams.edu" class="footer-wordmark">University of Arkansas for Medical Sciences</a>
+
+<!--         <h4>Connect with us:</h4>
+
+        <nav aria-label="social networking">
+            <ul class="footer-social">
+                <li><a class="facebook" href="http://facebook.com/uamshealth">Facebook</a></li>
+                <li><a class="twitter" href="http://twitter.com/uamshealth">Twitter</a></li>
+                <li><a class="instagram" href="http://instagram.com/uamshealth">Instagram</a></li>
+                <li><a class="youtube" href="https://www.youtube.com/user/UAMSHealth">YouTube</a></li>
+                <li><a class="linkedin" href="https://www.linkedin.com/company/uams">LinkedIn</a></li>
+                <li><a class="pinterest" href="http://pinterest.com/uamshealth">Pinterest</a></li>
+            </ul>
+        </nav> -->
+
+        <nav aria-label="footer links">
+            <ul class="footer-links">
+<!--                 <li><a href="http://www.uams.edu/accessibility">Accessibility</a></li> -->
+                <li><a href="http://uamshealth.com/disclaimer/">Disclaimer</a></li>
+                <li><a href="http://jobs.uams.edu">Jobs</a></li>
+                <li><a href="http://uamshealth.com/privacy/#legal">Copyright Statement</a></li>
+                <li><a href="http://uamshealth.com/privacy/">Privacy</a></li>
+                <li><a href="http://uamshealth.com/termsofuse/">Terms</a></li>
+            </ul>
+        </nav>
+
+        <p>&copy; <?php echo date("Y"); ?> University of Arkansas for Medical Sciences  |  Little Rock, AR</p>
+
+    </div>
+
+    </div><!-- #uams-container-inner -->
+    </div><!-- #uams-container -->
+
+<?php wp_footer(); ?>
+
+<?php
+    if ( get_post_meta( get_the_ID() , 'custom_footer_script' , 'true' ) )
+		echo get_post_meta( get_the_ID() , 'custom_footer_script' , 'true' );
+    ?>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,105 @@
+<?php
+/**
+* Add an automatic default custom taxonomy for calendar.
+* If no event (taxonomy) is set, the event will be sorted as “draft” and won’t return an offset error.
+*
+*/
+
+// Fix ACF Support
+add_filter('_includes/acf-pro/settings/show_admin', '__return_true');
+
+add_action('acf/input/admin_head', 'my_acf_admin_head');
+
+function my_acf_admin_head() {
+
+    ?>
+    <script type="text/javascript">
+    (function($) {
+
+        $(document).ready(function(){
+
+            $('.acf-field-573f70ac9357c .acf-input').append( $('#postdivrich') );
+
+        });
+
+    })(jQuery);
+    </script>
+    <style type="text/css">
+        .acf-field #wp-content-editor-tools {
+            background: transparent;
+            padding-top: 0;
+        }
+    </style>
+    <?php
+
+}
+
+register_sidebar( array(
+'name' => 'Footer Flex Widget',
+'id' => 'footer-flex-widget',
+'description' => 'Appears in the footer area',
+'before_widget' => '<section id="%1$s" class="widget %2$s">',
+'after_widget' => '</section>',
+'before_title' => '<h2 class="widget-title">',
+'after_title' => '</h3>',
+) );
+
+add_filter('body_class','uams_field_body_class');
+function uams_field_body_class( $classes ) {
+    $classes[] = 'uams-primary';
+
+    $site_url = get_bloginfo( 'url' );
+    //$classes[] = $site_url;
+
+    if(strpos($site_url, 'cancer') !== false) {
+        $classes[] = 'cancer';
+    } elseif (strpos($site_url, 'myeloma') !== false) {
+        $classes[] = 'myeloma';
+    } elseif (strpos($site_url, 'aging') !== false) {
+        $classes[] = 'aging';
+    } elseif (strpos($site_url, 'pri') !== false) {
+        $classes[] = 'pri';
+    } elseif (strpos($site_url, 'tri') !== false) {
+        $classes[] = 'tri';
+    } elseif (strpos($site_url, 'eye') !== false) {
+        $classes[] = 'eye';
+    } elseif (strpos($site_url, 'spine') !== false) {
+        $classes[] = 'spine';
+    }
+
+    // return the $classes array
+    return $classes;
+}
+
+
+//* Setup widget counts
+function footer_count_widgets( $id ) {
+    global $sidebars_widgets;
+
+    if ( isset( $sidebars_widgets[ $id ] ) ) {
+        return count( $sidebars_widgets[ $id ] );
+    }
+
+}
+
+function footer_widget_area_class( $id ) {
+
+    $count = footer_count_widgets( $id );
+
+    $class = '';
+    
+    if ( $count == 1 ) {
+        $class .= ' widget-full';
+    } elseif ( $count % 3 == 0 ) {
+        $class .= ' widget-thirds';
+    } elseif ( $count % 4 == 0 ) {
+        $class .= ' widget-fourths';
+    } elseif ( $count % 2 == 1 ) {
+        $class .= ' widget-halves uneven';
+    } else {    
+        $class .= ' widget-halves';
+    }
+
+    return $class;
+    
+}
